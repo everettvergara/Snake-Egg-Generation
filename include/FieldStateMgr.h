935ad17278b2329cdfadd4354386109fb318965d
Everett Gaius Vergara <evergara@shinra.com.ph>
2022-03-21 21:08:45 +0800

    reorg files

---
+++ include/FieldStateMgr.h
@@ -0,0 +1,111 @@
+/*
+ *  FieldStateMgr.h - Class to handle proper Snake Egg Generation
+ *  Copyright (C) 2022 Everett Gaius S. Vergara
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *	
+ */
+
+#ifndef _FIELD_STATE_MANAGER_H_
+#define _FIELD_STATE_MANAGER_H_
+
+// DEBUG ON = Commented #define NDEBUG 1
+// DEBUG OFF = Uncommented #define NDEBUG 1
+// #define NDEBUG 1
+
+#include <iostream>
+#include <iomanip>
+#include <cassert>
+
+#include "SnakeMisc.hpp"
+#include "Area.hpp"
+#include "Point.hpp"
+#include "FieldState.hpp"
+
+using namespace g80;
+
+namespace snake {
+
+    class FieldStateMgr {
+    public:
+        // Constructors, Assignments 
+        FieldStateMgr(Area area);
+        FieldStateMgr(const FieldStateMgr &) = delete;
+        FieldStateMgr(FieldStateMgr &&) = delete;
+        auto operator=(const FieldStateMgr &) -> FieldStateMgr & = delete;
+        auto operator=(FieldStateMgr &&) -> FieldStateMgr & = delete;
+        virtual ~FieldStateMgr();
+
+        // Helper to convert Points to Ix and vice-versa
+        auto get_ix(const Point &point) const -> Dim;
+        auto get_point(const Dim &ix) const -> Point;
+
+        // Check if used or free
+        auto is_point_used(const Point &point) const -> bool;
+        auto is_point_free(const Point &point) const -> bool;
+        auto is_ix_used(const Dim &ix) const -> bool;
+        auto is_ix_free(const Dim &ix) const -> bool;
+
+        // Set as used
+        auto set_point_as_used(const Point &point, const StateType &state_type) -> void;
+        auto set_ix_as_used(const Dim &ix, const StateType &state_type) -> void;
+        
+        // Set as free
+        auto set_point_as_free(const Point &point) -> void;
+        auto set_ix_as_free(Dim ix) -> void;
+
+        // Get next egg
+        auto get_next_egg_point() -> const PointOp;
+        auto get_next_egg_ix() -> const DimOp;
+
+        // Get state point
+        auto get_state_point(const Point &point) const -> Point;
+        auto get_state_ix(const Dim &ix) const -> Dim;
+        
+        // Get Mapper
+        auto get_mapper_point(const Point &point) const -> Point;
+        auto get_mapper_ix(const Dim &ix) const -> Dim;
+
+        // Get current egg position
+        auto get_egg_point() const -> Point;
+        auto get_egg_ix() const -> Dim;
+        auto has_egg() const -> bool;
+
+        // Dims
+        auto height() const -> Dim;
+        auto width() const -> Dim;
+
+        // Get State Type
+        auto get_state_type_point(const Point &point) const -> const StateType &;
+        auto get_state_type_ix(const Dim &ix) const -> const StateType &;
+        
+        // Debuggers
+        auto debug() -> void;
+        auto show() -> void;
+
+        // INVALIDS
+        auto NOT_FREE_POINT() const -> Point;
+        auto NOT_FREE_IX() const -> Dim;
+
+    protected:
+        Area area_;
+        Dim div_;
+        Dim egg_;
+        Fields state_;
+        Fields mapper_;
+        FieldStates type_;
+    };
+}
+
+#endif
---
+++ src/FieldStateMgr.cpp
@@ -0,0 +1,180 @@
+#include "FieldStateMgr.h"
+
+
+snake::FieldStateMgr::FieldStateMgr(Area area) : 
+    area_(area),
+    div_(area_()), 
+    egg_(area_()),
+    state_(std::make_unique<Dim[]>(area_())),
+    mapper_(std::make_unique<Dim[]>(area_())),
+    type_(std::make_unique<StateType[]>(area_())) {
+    for (Dim i = 0; i < area_(); ++i) {
+        state_.get()[i] = i;
+        mapper_.get()[i] = i;
+        type_.get()[i] = FREE;
+    }
+}
+
+auto snake::FieldStateMgr::get_ix(const Point &point) const -> Dim {
+    return point.y * area_.w + point.x;
+}
+
+auto snake::FieldStateMgr::get_point(const Dim &ix) const -> Point {
+    return {static_cast<Dim>(ix % (area_.w)), static_cast<Dim>(ix / (area_.w))};
+}
+
+auto snake::FieldStateMgr::is_point_used(const Point &point) const -> bool {
+    return is_ix_used(get_ix(point));
+}
+
+auto snake::FieldStateMgr::is_ix_used(const Dim &ix) const -> bool {
+    return mapper_[ix] >= div_;
+}
+
+
+auto snake::FieldStateMgr::NOT_FREE_POINT() const -> Point {
+    return get_point(NOT_FREE_IX());
+}
+
+auto snake::FieldStateMgr::NOT_FREE_IX() const -> Dim {
+    return area_();
+}
+
+auto snake::FieldStateMgr::set_point_as_used(const Point &point, const StateType &state_type) -> void {
+    set_ix_as_used(get_ix(point), state_type);
+}
+
+auto snake::FieldStateMgr::set_ix_as_used(const Dim &ix, const StateType &state_type) -> void {
+    assert(is_ix_free(ix));
+    assert(state_type > FREE);
+
+    --div_;
+
+    // Visualization:
+    //
+    // mapper_[]
+    // ix:  0   1   2   3   4   5   ...
+    // val: 3  ... ... ... ... ... ...
+    //      |
+    //      +-----------+
+    //                  |
+    // state_[]         v
+    // ix:  0   1   2   3   4   5   ...
+    // val  ... ... ... 0   ... ... ...
+    //                        | |
+    //                        | | div_
+    //                        v v
+    //      <----- free ----->| | <--- used --->
+    //                  
+
+    auto mapper_ix_in_state = mapper_[ix];
+    auto ix_free = state_[div_];
+    std::swap(state_[mapper_[ix]], state_[div_]);
+    mapper_[ix] = div_;
+    mapper_[ix_free] = mapper_ix_in_state;
+    type_[ix] = state_type;
+}
+
+auto snake::FieldStateMgr::set_point_as_free(const Point &point) -> void {
+    set_ix_as_free(get_ix(point));
+}
+
+auto snake::FieldStateMgr::set_ix_as_free(Dim ix) -> void {
+    assert(!is_ix_free(ix));
+    auto mapper_ix_in_state = mapper_[ix];
+    auto ix_used = state_[div_];
+    std::swap(state_[mapper_[ix]], state_[div_]);
+    mapper_[ix] = div_;
+    mapper_[ix_used] = mapper_ix_in_state;
+    type_[ix] = FREE;
+    ++div_;
+}
+
+auto snake::FieldStateMgr::get_next_egg_point() -> const PointOp {
+    if(!get_next_egg_ix().has_value())
+        return {};
+    return get_point(egg_);
+} 
+
+auto snake::FieldStateMgr::get_next_egg_ix() -> const DimOp {
+    if (div_ == 0) 
+        return {};
+    egg_ = state_[rand() % div_];
+    type_[egg_] = EGG;
+    return egg_;
+} 
+
+auto snake::FieldStateMgr::get_state_ix(const Dim &ix) const -> Dim {
+    return state_[ix];
+}
+
+auto snake::FieldStateMgr::get_state_point(const Point &point) const -> Point {
+    return get_point(get_state_ix(get_ix(point)));
+}
+
+auto snake::FieldStateMgr::get_mapper_point(const Point &point) const -> Point {
+    return get_point(get_mapper_ix(get_ix(point)));
+}
+
+auto snake::FieldStateMgr::get_mapper_ix(const Dim &ix) const -> Dim {
+    return mapper_[ix];
+}
+
+auto snake::FieldStateMgr::is_point_free(const Point &point) const -> bool {
+    return is_ix_free(get_ix(point));
+}
+
+auto snake::FieldStateMgr::is_ix_free(const Dim &ix) const -> bool {
+    return mapper_[ix] < div_;
+}
+
+auto snake::FieldStateMgr::get_egg_point() const -> Point {
+    return get_point(egg_);
+}
+
+auto snake::FieldStateMgr::get_egg_ix() const -> Dim {
+    return egg_;
+}
+
+auto snake::FieldStateMgr::has_egg() const -> bool {
+    return egg_ < area_();
+}
+
+auto snake::FieldStateMgr::height() const -> Dim {
+    return area_.h;
+}
+
+auto snake::FieldStateMgr::width() const -> Dim {
+    return area_.w;
+}
+
+auto snake::FieldStateMgr::get_state_type_point(const Point &point) const -> const StateType & {
+    return get_state_type_ix(get_ix(point));
+}
+
+auto snake::FieldStateMgr::get_state_type_ix(const Dim &ix) const -> const StateType & {
+    return type_[ix];
+}
+
+auto snake::FieldStateMgr::debug() -> void {
+    std::cout << "\n#     :";
+    for (int i = 0; i < area_(); ++i)
+        std::cout << std::setw(4) << i;
+    std::cout << "\nDivider:" << std::setw(4 * (div_ + 1) - 1) << "^";
+    std::cout << "\nF & U  :";
+    for (int i = 0; i < area_(); ++i)
+        std::cout << std::setw(4) << state_[i];
+    std::cout << "\nState :";
+    for (int i = 0; i < area_(); ++i)
+        std::cout << std::setw(4) << mapper_[i];
+    std::cout << "\n\n";
+}
+
+auto snake::FieldStateMgr::show() -> void {
+    for (Dim i = 0; i < area_.h; ++i) {
+        for (Dim j = 0; j < area_.w; ++j) {
+            std::cout << (is_point_free({j, i}) ? ' ' : '#');
+        }
+        std::cout << "\n";
+    }
+}
